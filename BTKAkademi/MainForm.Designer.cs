2e17079b7d9c28a45044c711c478f51f6de1355e
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-04-22 22:15:34 +0300

    Interface kullanilarak personmanager i customer veya student olarak implemet etmeyi denedik.

---
+++ BTKAkademi/MainForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace BTKAkademi
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bntExit = new DevExpress.XtraEditors.SimpleButton();
+            this.btnEkle = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSil = new DevExpress.XtraEditors.SimpleButton();
+            this.SuspendLayout();
+            // 
+            // bntExit
+            // 
+            this.bntExit.Location = new System.Drawing.Point(739, 307);
+            this.bntExit.Name = "bntExit";
+            this.bntExit.Size = new System.Drawing.Size(94, 29);
+            this.bntExit.TabIndex = 0;
+            this.bntExit.Text = "Kapa";
+            this.bntExit.Click += new System.EventHandler(this.bntExit_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(200, 307);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(136, 29);
+            this.btnEkle.TabIndex = 1;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(566, 307);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(94, 29);
+            this.btnSil.TabIndex = 2;
+            this.btnSil.Text = "Sil";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1257, 450);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.bntExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton bntExit;
+        private DevExpress.XtraEditors.SimpleButton btnEkle;
+        private DevExpress.XtraEditors.SimpleButton btnSil;
+    }
+}
+
---
+++ BTKAkademi/MainForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BTKAkademi
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            PersonManager person = new PersonManager();
+            person.Add(new Customer { id = 1, ad = "ayse", soyad = "kilic" });
+        }
+
+        private void bntExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+    interface IPerson
+    {
+        int id { get; set; }
+        string ad { get; set; }
+        string soyad { get; set; }
+    }
+    class Customer : IPerson
+    {
+        public int id { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+    }
+    class Student : IPerson
+    {
+        public int id { get; set; }
+        public string ad { get; set; }
+        public string soyad { get; set; }
+    }
+    class PersonManager
+    {
+        public void Add(IPerson customer)
+        {
+            MessageBox.Show("eklendi = " + customer.ad);
+        }
+    }
+}
